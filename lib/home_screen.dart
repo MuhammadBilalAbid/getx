6c973e415c26e65b5442661d79e37612a6b4294c
Muhammad Bilal <mbilalabid800@.com>
2024-11-05 12:39:16 +0500

    5-11

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,22 +15,61 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text("Getx SnackBar"),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: () {
-          Get.snackbar(
-            "Asif taj",
-            "Subscribe my channale",
-            colorText: Colors.white,
-            backgroundColor: Colors.black,
-            snackPosition: SnackPosition.TOP,
-          );
-        },
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
+      body: Column(
+        children: [
+          Card(
+            color: Colors.greenAccent,
+            child: ListTile(
+              title: const Text("Getx dialoge alert"),
+              subtitle: const Text("Getx dialoge alert"),
+              onTap: () {
+                Get.bottomSheet(Container(
+                  decoration: BoxDecoration(
+                    color: Colors.greenAccent,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 12),
+                      ListTile(
+                        leading: const Icon(Icons.light_mode),
+                        title: const Text("Light mode"),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.light());
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.dark_mode),
+                        title: const Text("Dark mode"),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.dark());
+                        },
+                      )
+                    ],
+                  ),
+                ));
+              },
+            ),
+          )
+        ],
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Colors.black,
+      //   onPressed: () {
+      //     Get.snackbar(
+      //       onTap: (snack) {},
+      //       "Asif taj",
+      //       "Subscribe my channale",
+      //       colorText: Colors.white,
+      //       backgroundColor: Colors.black,
+      //       snackPosition: SnackPosition.TOP,
+      //     );
+      //   },
+      //   child: const Icon(
+      //     Icons.add,
+      //     color: Colors.white,
+      //   ),
+      // ),
     );
   }
 }
